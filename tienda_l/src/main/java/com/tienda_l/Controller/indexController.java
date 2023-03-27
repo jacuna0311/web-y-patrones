fe6bef0324672bf048bef6abf3f5dec80e51c7d2
jacuna0311 <acunamorajuandavid@gmail.com>
2023-03-27 10:27:37 -0600

    Semana_10

---
+++ tienda_l/src/main/java/com/tienda_l/Controller/indexController.java
@@ -0,0 +1,34 @@
+package com.tienda_l.controller;
+
+
+import com.tienda_l.domain.Cliente;
+import com.tienda_l.service.ArticuloService;
+import com.tienda_l.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Service
+@Controller
+public class indexController {
+    
+    @Autowired
+    private ArticuloService articuloService;
+    
+    
+    @GetMapping("/")
+    public String inicio(Model model) { 
+        
+        var articulos=articuloService.getArticulos(true);
+        model.addAttribute("articulos", articulos);
+        
+      
+        return "index";
+    }
+
+    
+    
+}
