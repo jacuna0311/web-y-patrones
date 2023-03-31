5d0ae991f5fcab831ffbaa5e5f09cfbfdd83b66b
jacuna0311 <acunamorajuandavid@gmail.com>
2023-03-31 10:20:09 -0600

    Sermana11

--- tienda_l/src/main/java/com/tienda_l/SecurityConfig.java
+++ tienda_l/src/main/java/com/tienda_l/SecurityConfig.java
@@ -1,14 +1,15 @@
-
 package com.tienda_l;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -24,15 +25,54 @@ public class SecurityConfig{
              UserDetails sales=User.builder()
             .username("rebeca")
             .password("{noop}456")
-            .roles("VENDEDOR","ADMIN")
+            .roles("VENDEDOR","USER")
             .build();
               UserDetails user=User.builder()
             .username("pedro")
             .password("{noop}789")
-            .roles("ADMIN")
+            .roles("USER")
             .build();
              return new InMemoryUserDetailsManager(user,sales,admin);
     }   
+    
+    
+    @Bean
+    
    
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+    throws Exception {
+        http
+                .authorizeHttpRequests((request) -> request
+                        .requestMatchers("/",
+                                "/index",
+                                "/errores/**",
+                                "/webjars/**").permitAll()
+                        .requestMatchers(
+                                "/articulo/nuevo",
+                                "/articulo/guardar",
+                                "/articulo/modificar/**",
+                                "/articulo/eliminar/**",
+                                "/categoria/nuevo",
+                                "/categoria/guardar",
+                                "/categoria/modificar/**",
+                                "/categoria/eliminar/**",
+                                "/cliente/nuevo",
+                                "/cliente/guardar",
+                                "/cliente/modificar/**",
+                                "/cliente/eliminar/**"
+                                ).hasRole("ADMIN")
+                        .requestMatchers(
+                                "/articulo/listado",
+                                "/categoria/listado",
+                                "/cliente/listado"
+                        ).hasAnyRole("ADMIN","VENDEDOR")
+                )
+                .formLogin((form) -> form
+                .loginPage("/login").permitAll())
+                .logout((logout) -> logout.permitAll())
+                .exceptionHandling()
+                .accessDeniedPage("/errores/403");
+        return http.build();
+    }
     
 }
